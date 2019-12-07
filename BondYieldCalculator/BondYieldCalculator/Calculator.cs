9e0f331f79adfd267eb5961a32bace1fe758f588
xiatian52673 <xiatian52673@gmail.com>
2019-12-07 10:38:57 -0500

    resolve dependency issue

---
+++ BondYieldCalculator/BondYieldCalculator/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace BondYieldCalculator
+{
+    public class Calculator
+    {
+        public double CalcPrice(double coupon, int years, double face, double rate)
+        {
+            double price = 0, payment = face * coupon;
+            for (int year = 1; year <= years; year++)
+            {
+                price += payment / (Math.Pow(1 + rate, year));
+            }
+            price += face / (Math.Pow(1 + rate, years));
+            return Math.Round(price,7);
+        }
+
+        public double CalcYield(double coupon, int years, double face, double price)
+        {
+            double payment = coupon * face, epsilon = 1;
+            double rate = (payment + (face - price) / years) / ((face + price) / 2);
+            double estPrice = CalcPrice(coupon, years, face, rate);
+            while (Math.Abs(estPrice - price) > 0.0000001)
+            {
+                if (estPrice > price)
+                {
+                    rate += epsilon;
+                    estPrice = CalcPrice(coupon, years, face, rate);
+                }
+                else
+                {
+                    rate -= epsilon;
+                    estPrice = CalcPrice(coupon, years, face, rate);
+                }
+                epsilon /= 2;
+            }
+            return Math.Round(rate, 7);
+        }
+
+    }
+
+}
---
+++ BondYieldCalculator/BondYieldCalculator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace BondYieldCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+            Console.WriteLine("Bond Yeild Calculator Console Application in C#\r");
+            Console.WriteLine("-----------------------------------------------\n");
+
+            while (!endApp)
+            {
+                Console.WriteLine("Type coupon, and then press Enter");
+                double coupon = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Type years, and then press Enter");
+                int years = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Type face, and then press Enter");
+                double face = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Choose an option from the following list:");
+                Console.WriteLine("\tp - Calculate Price");
+                Console.WriteLine("\ty - Calculate Yeild");
+                Console.Write("Your option? ");
+
+                Calculator calculator = new Calculator();
+
+
+                switch (Console.ReadLine())
+                {
+                    case "p":
+
+                        Console.WriteLine("Type rate, and then press Enter");
+                        double rate = Convert.ToDouble(Console.ReadLine());
+                        DateTime start = DateTime.Now;
+                        Console.WriteLine($"Your result= " + calculator.CalcPrice(coupon, years, face, rate));
+                        TimeSpan time = DateTime.Now - start;
+                        Console.WriteLine("Time Cost:" + String.Format("{0}.{1}", time.Seconds, time.Milliseconds.ToString().PadLeft(3, '0')) + "Seconds");
+                        break;
+                    case "y":
+
+                        Console.WriteLine("Type price, and then press Enter");
+                        double price = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine($"Your result= " + calculator.CalcYield(coupon, years, face, price));
+                        break;
+                }
+                Console.WriteLine("------------------------\n");
+
+                // Wait for the user to respond before closing.
+                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                endApp |= Console.ReadLine() == "n";
+
+                Console.WriteLine("\n");
+            }
+            return;
+
+
+        }
+    }
+}
